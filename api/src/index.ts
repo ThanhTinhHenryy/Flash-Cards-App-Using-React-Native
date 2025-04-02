8f4d2aa5da18102cfb2efe616336e50254cd4b48
ThanhTinhHenry <ThanhTinh.Henry@gmail.com>
2025-04-02 17:08:47 +0700

    config va chay duoc co ban

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,15 @@
-import express, { application } from "express";
-import dotenv from "dotenv";
-dotenv.config();
+import express, { Request, Response } from "express";
 
+import dotenv from "dotenv";
+
+dotenv.config();
 // * Cau hinh port tu file .env || PORT = 3000
-const { PORT } = process.env || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.get("/", (req: Request, res: Response) => {
+  res.json({ ThanhTinh: true });
+});
 app.listen(PORT, () => {
   console.log(`Đang lắng nghe trên cổng: ${PORT}`);
 });
